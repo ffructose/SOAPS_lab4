5edb1549d571f8c3f1c4a1a622c756c1a53c58b0
Anna Hnatiuk <losers2004@gmail.com>
2025-04-09 23:09:53 +0200

    meowk

--- go-micro/front-end/cmd/web/main.go
+++ go-micro/front-end/cmd/web/main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -12,27 +13,53 @@ func main() {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting front end service on port 8080")
+	// НОВИЙ МАРШРУТ ЕБАУТ
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "about.page.gohtml")
+	})
+
+	// МАРШРУТ ФОРМИ
+	http.HandleFunc("/form", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "form.page.gohtml")
+	})
+
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Redirect(w, r, "/form", http.StatusSeeOther)
+			return
+		}
+		r.ParseForm()
+		pes_amount := r.FormValue("pes_amount")
+		fmt.Fprintf(w, "Ого, у вас %s песів, це круто!", pes_amount)
+	})
+
+	fmt.Println("Running on http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 }
 
 func render(w http.ResponseWriter, t string) {
-	partials := []string{
-		"./cmd/web/templates/base.layout.gohtml",
-		"./cmd/web/templates/header.partial.gohtml",
-		"./cmd/web/templates/footer.partial.gohtml",
+	fmt.Println("Rendering:", t)
+	files := []string{
+		"cmd/web/templates/base.layout.gohtml",
+		"cmd/web/templates/header.partial.gohtml",
+		"cmd/web/templates/footer.partial.gohtml",
+		fmt.Sprintf("cmd/web/templates/%s", t),
 	}
-
-	templateSlice := append([]string{fmt.Sprintf("./cmd/web/templates/%s", t)}, partials...)
-
-	tmpl, err := template.ParseFiles(templateSlice...)
+	tmpl, err := template.ParseFiles(files...)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println("Parse error:", err)
+		http.Error(w, "Template error", 500)
 		return
 	}
+	data := map[string]interface{}{
+		"CurrentTime": time.Now().Format("02 Jan 2006 15:04:05"),
+	}
 
-	tmpl.Execute(w, nil)
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Println("Execute error:", err)
+	}
 }
